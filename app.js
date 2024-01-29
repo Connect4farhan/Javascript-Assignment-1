4c4447554122a4f7ee949539302f2568af011530
Farhan Ahmed <153209469+Connect4farhan@users.noreply.github.com>
2024-01-29 23:45:39 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,242 +1,198 @@
 
-Q-01 :  Write a script to greet your website visitor using JS alert box. (Welcome to
-        Our Website)
-Ans  :  alert("Welcome to Our Website");
+//Question-1
+// alert("Welcome to Our Website");
 
 
 
-Q-02 :  Write a script to display message on your web page. (Error! Please Enter a
-        valid password)
-
-Ans  :  alert("Error! Please Enter a valid password");
+//Question-2
+// alert("Error! Please Enter a valid password");
 
 
 
-Q-03 :  Write a script to display message on your web page: (Hint: Use line break)
-
-Ans  :  alert("Hint: This message is displayed\nwith line breaks.");
-
-
-
-Q-04 :  Write a script to display messages in sequence (3 Alerts)
-
-Ans  :  window.alert("Welcome to Our Website");
-        window.alert("I have working the first time in Java-script.");
-        window.alert("Thank You So Much");
+//Question-3
+// alert("Hint: This message is displayed\nwith line breaks.");
 
 
 
-Q-05 :  Make use of alerts in your new/existing HTML & CSS project.
-Ans  :  
+//Question-4
+// window.alert("Welcome to Our Website");
+// window.alert("I have working the first time in Java-script.");
+// window.alert("Thank You So Much");
 
 
 
-Q-06 :  6. Practice placement of element in following sections of your project in
-        exercise 5:
-        a. Head
-        b. Body (before your page’s HTML)
-        c. Body (inside your page’s HTML)
-        d. Body (after your page’s HTML)
-Ans  :  
+
+//Question-7
+// var username = "username";
 
 
 
-Q-07 :  Declare a variable called username.
-
-Ans  :  var username = "username";
-
-
-
-Q-08 :  Declare a variable called myName & assign to it a string that represents your Full Name.
-
-Ans  :  var myname = "Farhan Ahmed";
+//Question-8
+// var myname = "Farhan Ahmed";
 
 
 
-Q-09 :  Write script to
-        a) Declare a JS variable, titled message.
-        b) Assign “Hello World” to variable message
-        c) Display the message in alert box.
-
-Ans  :  var title_message = "Hello World";
-        alert(title_message);
+//Question-9
+// var title_message = "Hello World";
+// alert(title_message);
 
 
 
-Q-10 :  Write a script to save student’s bio data in JS variables and show the data in
-        alert boxes.
-
-        Ans  :  var student_name = "Farhan Ahmed";
-            father_name = "Muhammad Shafi";
-            student_id =  "WD1-M-42277";
-        alert(student_name);    
-        alert(father_name);
-        alert(student_id);
+//Question-10
+// var student_name = "Farhan Ahmed";
+//     father_name = "Muhammad Shafi";
+//     student_id =  "WD1-M-42277";
+// alert(student_name);    
+// alert(father_name);
+// alert(student_id);
 
 
 
-Q-11 :  Declare a variable called email and assign to it a string that represents your
-        Email Address(e.g. example@example.com). Show the blow mentioned
-        message in an alert box.(Hint: use string concatenation)
-
-Ans  :  var email = "connect4farhanahmed@gmail.com";
-        alert(email);        
+//Question-11
+// var email = "connect4farhanahmed@gmail.com";
+// alert(email);        
 
 
 
-Q-12 :  Write a script to display this in browser through JS
-
-Ans  :  <script>
-        document.write("Hello friend! my name is Farhan Ahmed");
-        </script>
+//Question-12
+// document.write("Hello friend! my name is Farhan Ahmed");
 
 
 
-Q-13 :  Store following string in a variable and show in alert and browser through JS 
 
-Ans  :  var frame = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
-        alert = (frame);
-        document.write( "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
-
-
-
-Q-14 :  Declare a variable called age & assign to it your age. Show your age in an alert box.
-
-Ans  :  var age = 30;
-        alert(age);
+//Question-13 
+// var frame = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert = (frame);
+// document.write( "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
 
 
 
-Q-15 :  Declare & initialize a variable to keep track of how many times a visitor has
-        visited a web page. Show his/her number of visits on your web page. For
-        example: “You have visited this site N times”
-
-Ans  :  var visit = " 456 time."
-        message = "You have visited this site:" + visit;
-        alert(message);
+//Question-14
+// var age = 30;
+// alert(age);
 
 
 
-Q-16 :  Declare a variable called birthYear & assign to it your birth year. Show the
-        following message in your browser
+//Question-15
+// var visit = " 456 time."
+// message = "You have visited this site:" + visit;
+// alert(message);
 
-Ans  :  var birthyear = "My Birth-year is. 1993"
-        document.write(birthyear);
+
+
+//Question-16
+// var birthyear = "My Birth-year is. 1993"
+// document.write(birthyear);
       
 
 
-Q-17 :  A visitor visits an online clothing store www.xyzClothing.com . Write a script
-        to store in variables the following information: a. Visitor’s name b. Product
-        title c. Quantity i.e. how many products a visitor wants to order Show the
-        following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ
-        Clothing store”.
-
-Ans  :  var visitorsname = "John Doe",
-        Producttitle = "T-shirt(s)",
-        quantity = 50;
-        document.write(visitorsname),
-        document.write(Producttitle),
-        document.write(quantity);
+//Question-17
+// var visitorsname = "John Doe",
+// Producttitle = "T-shirt(s)",
+// quantity = 50;
+// document.write(visitorsname),
+// document.write(Producttitle),
+// document.write(quantity);
 
 
 
-Q-18 :  Declare 3 variables in one statement.
-Ans  :  var name = "Farhan Ahmed"
-            fname = "Muhammad Shafi"
-            celphone = +923122368181      
+//Question-18
+// var name = "Farhan Ahmed"
+// fname = "Muhammad Shafi"
+// celphone = +923122368181      
 
 
 
-Q-19 :  Declare 5 legal & 5 illegal variable names. 
-Ans  :  legal variable:
-                1)camelLatter, 2)snake_latter, 3)Underscore, 4)Dollar Sign, 5)A (Number) cannot be the initial character
-        illegal variable:
-                1)Space, 2)KeyWord, 3)Symbol, 4)punctuation, 5)special characters
+//Question-19
+// legal variable:
+//         1)camelLatter, 2)snake_latter, 3)Underscore, 4)Dollar Sign, 5)A (Number) cannot be the initial character
+// illegal variable:
+//         1)Space, 2)KeyWord, 3)Symbol, 4)punctuation, 5)special characters
 
 
 
 
-Q-20 :  Display this in your browser
-        a) A heading stating “Rules for naming JS variables”
-        b) Variable names can only contain ______, ______, ______ and ______. For
-        example $my_1stVariable
-        c) Variables must begin with a ______, ______ or _____. For example
-        $name, _name or name
-        d) Variable names are case _________
-        e) Variable names should not be JS ________
-
-Ans  :  document.write("Rules for naming JS variables");
-        document.write("<br>Variable names can only contain Alphabet,Digits,Under Score and Dollar Sign.");
-        document.write("<br>Variables must begin with a Alphabet,Digits or Under Score.");
-        document.write("<br>Variable names should not be JS space");   
+//Question-20
+// document.write("Rules for naming JS variables");
+// document.write("<br>Variable names can only contain Alphabet,Digits,Under Score and Dollar Sign.");
+// document.write("<br>Variables must begin with a Alphabet,Digits or Under Score.");
+// document.write("<br>Variable names should not be JS space");   
 
 
 
-Q-21 :  Write a program that take two numbers & add them in a new variable.
-        Show the result in your browser.
-
-Ans  :  var num1 = 10;
-        var num2 = 30;
-        alert (num1 + num2);
+//Question-21
+// var num1 = 10;
+// var num2 = 30;
+// alert (num1 + num2);
 
 
 
-Q-22 :  Repeat task21 for subtraction, multiplication, division & modulus
-Ans  :  var num = 10;
-        alert (num + 30);
-        alert (num * 30);
-        alert (num / 30);
-        alert (num % 30);
+//Question-22
+// var num = 10;
+// alert (num + 30);
+// alert (num * 30);
+// alert (num / 30);
+// alert (num % 30);
 
 
 
-Q-23 :  Do the following using JS Mathematic Expressions
-a. Declare a variable.
-b. Show the value of variable in your browser like “Value after variable
-declaration is: ??”.
-c. Initialize the variable with some number.
-d. Show the value of variable in your browser like “Initial value: 5”.
-e. Increment the variable.
-f. Show the value of variable in your browser like “Value after
-increment is: 6”.
-g. Add 7 to the variable.
-h. Show the value of variable in your browser like “Value after addition
-is: 13”.
-i. Decrement the variable.
-j. Show the value of variable in your browser like “Value after
-decrement is: 12”.
-k. Show the remainder after dividing the variable’s value by 3.
-l. Output : “The remainder is : 0”        
+//Question-23
+// a. Declare a variable.
+var num;
 
-// var num = 8;
-// alert (num);
+// b. Show the value of the variable in your browser like “Value after variable declaration is: ??”.
+document.write( num + "<br>");
 
-// var num1 = 12+5;
-// alert (num1);
+// c. Initialize the variable with some number.
+num = 5;
 
-// var num2 = 17;
+// d. Show the value of the variable in your browser like “Initial value: 5”.
+document.write(num + "<br>");
 
-Q-24 :  Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a
-        variable & calculate the cost of buying 5 tickets to a movie
+// e. Increment the variable.
+num++;
 
-Ans  :  var movieticket = 600;
-        var buyingticket = 5;
-        alert (movieticket * buyingticket);
+// f. Show the value of the variable in your browser like “Value after increment is: 6”.
+document.write(num + "<br>");
+
+// g. Add 7 to the variable.
+num += 7;
+
+// h. Show the value of the variable in your browser like “Value after addition is: 13”.
+document.write(num + "<br>");
+
+// i. Decrement the variable.
+num--;
+
+// j. Show the value of the variable in your browser like “Value after decrement is: 12”.
+document.write(num + "<br>");
+
+// k. Show the remainder after dividing the variable’s value by 3.
+var remainder = num % 3;
+
+// l. Output: “The remainder is: 0”
+document.write(remainder);
+
+
+
+//Question-24
+// var movieticket = 600;
+// var buyingticket = 5;
+// alert (movieticket * buyingticket);
 
         
 
 
-Q-25 :  Write a script to display multiplication table of any number in your browser.
-
-Ans  :  var i = 1;
-        while(i<51)
-        {
-           if(i%5==0)
-        {
-           document.write(i + "<br>");
-        }
-           i++;
-        }
+//Question-25
+// var i = 1;
+//         while(i<51)
+// {
+//         if(i%5==0)
+// {
+//         document.write(i + "<br>");
+// }
+//         i++;
+// }
    
    
   
